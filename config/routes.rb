b8403ab5de805f4ea259ffacd4bd7b948ea1f897
Gorzata96 <110gosia@gmail.com>
2017-03-12 14:46:53 +0100

    Add strona glowna

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :submissions
   post '/accept/:id', to: 'submissions#accept'
   #/ niekonieczny, ale lepiej widać, że to link; kontroler#nazwa akcji
-
+  root to: "submissions#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
